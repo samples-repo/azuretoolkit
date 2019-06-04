4487b20d60c5d0fb7f5a399bdcb1ddc30ca97102
Purna Chandra Panda <pupanda@outlook.com>
2019-06-04 21:29:18 +0530

    commit

--- ClientApp/src/app/common/common.module.ts
+++ ClientApp/src/app/common/common.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CognitiveService } from './services/cognitive.service';
 import { AzureHttpClient } from './services/azureHttpClient';
+import { UserService } from './services/user.service';
 
 @NgModule({
-    providers: [AzureHttpClient, CognitiveService]
+    providers: [AzureHttpClient, CognitiveService, UserService]
 })
 
 export class CommonModule { }
--- ClientApp/src/app/components/home/home.component.ts
+++ ClientApp/src/app/components/home/home.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../common/services/user.service';
+import { User } from '../../common/models/user';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
+
+export class HomeComponent implements OnInit {
+  user: User;
+
+  constructor(private userService: UserService) { }  
+
+  ngOnInit(): void {
+    this.userService.getUser().subscribe(user => this.user = user );
+  }
+  
 }
